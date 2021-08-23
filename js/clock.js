4f3e2011c5ceebc0a0cb46a30f2de56df886d80e
yevgeniamaksymiv <evgeniamaksymiv@gmail.com>
2021-08-23 11:10:46 +0000

    added js clock

---
+++ js/clock.js
@@ -0,0 +1,6 @@
+ setInterval(updateTime, 1000);
+ function updateTime() {
+    const clockContainer = document.querySelector('.clock');
+     clockContainer.innerText = new Date().toLocaleTimeString();
+    }
+
