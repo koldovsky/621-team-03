eb113bb2e0612f524a704d76ce0e18fdfd3ecf1c
VladyslavLohynskyi <watcher.good2003@gmail.com>
2021-08-25 20:46:54 +0300

    upgareted accordion in js file

--- js/acordion-review.js
+++ js/acordion-review.js
@@ -1,9 +1,15 @@
 (function () {
-    const accordion = document.getElementsByClassName("accordion");
-    for (let i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener("click", function () {
-            this.classList.toggle("active");
+    const accordions = document.getElementsByClassName("accordion");
+    for (let i = 0; i < accordions.length; i++) {
+        accordions[i].addEventListener("click", function () {
             let panel =  this.nextElementSibling;
+            this.classList.toggle("active");
+            for(let j = 0; j <accordions.length; j++){
+                if(j!==i){
+                    accordions[j].classList.remove("active");
+                    accordions[j].nextElementSibling.style.display = "none";
+                }
+            }
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
