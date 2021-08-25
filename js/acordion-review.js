77e21a72f4eb4402ee9a4bda51ad2bb050e30b6f
VladyslavLohynskyi <watcher.good2003@gmail.com>
2021-08-25 14:59:33 +0300

    changed acordion-faq (added acordion-review.js, added some styles for acordion in footer.css, replaced code in index.html)

---
+++ js/acordion-review.js
@@ -0,0 +1,14 @@
+(function () {
+    const accordion = document.getElementsByClassName("accordion");
+    for (let i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+})();
