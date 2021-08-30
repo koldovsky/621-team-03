bdcb71e858d73fcc58695e310ad087d26a983b9c
DanyloLipar <danilo.lipar2016@gmail.com>
2021-08-30 13:52:26 +0300

    Description website

--- js/itemlist-burger.js
+++ js/itemlist-burger.js
@@ -40,3 +40,13 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('pc');
 }
+
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu-body');
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle('lock')
+        iconMenu.classList.toggle('active');
+        menuBody.classList.toggle('active');
+    });
+}
