8e0ab64aa4e0d4bc7d7018225afd774bc3a83f8b
V-Klimonov <822775v@gmail.com>
2021-08-30 16:58:38 +0300

    fix carousel-items

--- js/carousel-items.js
+++ js/carousel-items.js
@@ -58,7 +58,7 @@
         const allFurnitureThings = [...furnitureThings];
         for (const thing of allFurnitureThings) {
             furnitureThingsHtml.push(`
-          <div class="carousel-items">
+          <div class="carousel-items-p">
           <a href="itemlist.html" class="carousel-image-thing"><img src="${thing.imgUrl}" class="carousel-img-s-card" alt="${thing.name}"></a>
           <h4 class="s-carousel-text text-positioning">${thing.name}</h4>
           <p class="s-carousel-product-price price-positioning">${thing.price} USD</p>
